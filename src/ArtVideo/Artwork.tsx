de7169e626da8aa56fa3b417ce3b478e5d35ccbf
Emerson Christie <emersonchristie86@gmail.com>
2021-05-25 07:25:20 -0700

    zoom and pan complete, need dynamic ratios

--- src/ArtVideo/Artwork.tsx
+++ src/ArtVideo/Artwork.tsx
@@ -1,56 +1,96 @@
-import {useEffect, useRef, useState} from 'react';
-import {interpolate, spring, useCurrentFrame, useVideoConfig} from 'remotion';
+import {useRef, useState} from 'react';
+import {Img, interpolate, useCurrentFrame, useVideoConfig} from 'remotion';
 import styled from 'styled-components';
 
 export const Artwork: React.FC<{artFile: String}> = ({artFile}) => {
+	const src = require('../static/artImages/art1.jpg');
+	// const src = require('../static/artImages/diptych.jpg');
+
 	const frame = useCurrentFrame();
 	const {height, width, fps, durationInFrames} = useVideoConfig();
 	const artRef = useRef();
 
 	const [state, setState] = useState({
-		artWidth: 0,
-		ratio: 0,
+		artWidth: 1,
+		artHeight: 1,
+		widthRatio: 1,
+		heightRatio: 1,
 	});
-
-	useEffect(() => {
-		const artWidth = artRef.current ? artRef.current.offsetWidth : 0;
-
-		setState({...state, artWidth, ratio: width / artWidth});
-
-		// console.log('width', artRef.current ? artRef.current.offsetWidth : 0);
-	}, []);
-
-	const progress = spring({
-		frame: frame,
-		from: 1,
-		to: 2,
-		fps,
-		config: {
-			mass: 4,
-			damping: 1000,
-		},
-	});
-
-	const scaleDriver = spring({
-		frame,
-		fps,
-		config: {
-			mass: 4,
-			damping: 1000,
-		},
-	});
-
-	const zoom = interpolate(scaleDriver, [0, 1], [1, state.ratio]);
 
 	const image = {
 		maxHeight: height * 0.75,
 		maxWidth: width * 0.75,
 	};
 
-	const src = require('../static/artImages/art1.jpg');
+	const onLoad = ({target: img}) => {
+		const artWidth = img.offsetWidth;
+		const artHeight = img.offsetHeight;
+		const screenWidth = window.innerWidth;
+		const screenHeight = window.innerHeight;
 
-	// const src = require('../static/artImages/' + artFile + '.jpg');
-	// console.log('🚀 ~ file: Artwork.tsx ~ line 17 ~ src', src);
+		setState({
+			...state,
+			artWidth,
+			artHeight,
+			widthRatio: screenWidth / artWidth,
+			heightRatio: screenHeight / artHeight,
+		});
+	};
+
+	// Zoom using current frame
+	const zoom = interpolate(
+		frame,
+		[
+			0,
+			(1 / 8) * durationInFrames, // zoom in until 1/8 through frames
+			(7 / 8) * durationInFrames, // zoom out starting at 7/8 duration
+			durationInFrames,
+		],
+		// [1, state.widthRatio * 2, state.widthRatio * 2, 1] // Using art width ratio
+		[1, 4, 4, 1] // Using standard scale
+	);
+
+	/*
+	0 = Begin zoom in to top left corner
+	1/8 = end zoom in to top left corner, begin pan down
+	1/4 
+	3/8 = Begin pan X right
+	1/2
+	5/8 = End pan X right, begin pan Y up
+	3/4 
+	7/8 = End pan Y up, begin Zoom out while pan X left back to center
+	**/
+
+	const panY = interpolate(
+		frame,
+		[
+			0,
+			(1 / 8) * durationInFrames,
+			(3 / 8) * durationInFrames,
+			(5 / 8) * durationInFrames,
+			(7 / 8) * durationInFrames,
+			durationInFrames,
+		],
+		[0, 0, 125, 125, 0, 0], // TODO find adequate ratio depending on images aspect ratio
+		{
+			extrapolateRight: 'clamp',
+		}
+	);
+	const panX = interpolate(
+		frame,
+		[
+			0,
+			(1 / 8) * durationInFrames,
+			(3 / 8) * durationInFrames,
+			(5 / 8) * durationInFrames,
+			(7 / 8) * durationInFrames,
+			durationInFrames,
+		],
+		[0, 0, 0, 125, 125, 0],
+		{
+			extrapolateRight: 'clamp',
+		}
+	);
 
 	const ArtContainer = styled.div`
 		display: flex;
@@ -59,25 +99,7 @@ export const Artwork: React.FC<{artFile: String}> = ({artFile}) => {
 		width: 100%;
 	`;
 
-	const Shadow = styled.div`
-		font-size: 0;
-		box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.2), 1px 2px 2px rgba(0, 0, 0, 0.2),
-			8px 16px 16px rgba(0, 0, 0, 0.2), 12px 24px 32px rgba(0, 0, 0, 0.2),
-			16px 32px 64px rgba(0, 0, 0, 0.2), 4px 8px 4px rgba(0, 0, 0, 0.09),
-			8px 16px 8px rgba(0, 0, 0, 0.09), 16px 32px 16px rgba(0, 0, 0, 0.09),
-			32px 64px 32px rgba(0, 0, 0, 0.09), 64px 128px 64px rgba(0, 0, 0, 0.09);
-		transform: scale(${zoom});
-		transform-origin: top;
-	`;
-
-	const imgStyle = {
-		maxHeight: `${image.maxHeight}px`,
-		width: 'auto',
-		maxWidth: `${image.maxWidth}px`,
-		height: 'auto',
-	};
-
-	const Art = styled.img`
+	const StyledImg = styled(Img)`
 		max-height: ${image.maxHeight}px;
 		width: auto;
 		max-width: ${image.maxWidth}px;
@@ -89,21 +111,18 @@ export const Artwork: React.FC<{artFile: String}> = ({artFile}) => {
 			8px 16px 8px rgba(0, 0, 0, 0.09), 16px 32px 16px rgba(0, 0, 0, 0.09),
 			32px 64px 32px rgba(0, 0, 0, 0.09), 64px 128px 64px rgba(0, 0, 0, 0.09);
 		transform: scale(${zoom});
-		transform-origin: top;
+		transform-origin: 25% 25%;
 	`;
 
-	// const onImgLoad = ({target: img}) => {
-	// 	const width = img.naturalWidth;
-	// 	const height = img.naturalHeight;
-
-	// console.log('Bounding Rect: ', artRef.current.getBoundingClientRect());
+	const TranslateDiv = styled.div`
+		transform: translate(-${panX}%, -${panY}%);
+	`;
 
 	return (
 		<ArtContainer>
-			{/* <Shadow ref={artRef}>
-				<Img style={imgStyle} src={src}></Img>
-			</Shadow> */}
-			<Art src={src} ref={artRef}></Art>
+			<TranslateDiv>
+				<StyledImg ref={artRef} src={src} onLoad={onLoad}></StyledImg>
+			</TranslateDiv>
 		</ArtContainer>
 	);
 };
--- src/Video.tsx
+++ src/Video.tsx
@@ -1,14 +1,30 @@
 import {Composition} from 'remotion';
 import {ArtVideo} from './ArtVideo';
 
+/*
+TODOS
+
+- Artwork props to pass to cli: image file, artist, title, year, dimensions
+- Config props: Video duration, focal points (most interesting parts of painting to zoom into)
+
+- 
+
+**/
+
 export const RemotionVideo: React.FC = () => {
+	// TODO add as CLI input props
+	const videoDuration = 120; // Duration in Seconds
+	const fps = 30; // frames per second
+
+	const frames = videoDuration * fps;
+
 	return (
 		<>
 			<Composition
 				id="ArtVideo"
 				component={ArtVideo}
-				durationInFrames={150}
-				fps={30}
+				durationInFrames={frames}
+				fps={fps}
 				width={1920}
 				height={1080}
 				defaultProps={{
