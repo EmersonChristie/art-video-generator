4b53a21f346ab83997be1fea5438fc40be73e3ed
Emerson Christie <emersonchristie86@gmail.com>
2021-06-01 11:21:25 -0700

    changed zoom and pan methods

--- src/ArtVideo/Artwork.tsx
+++ src/ArtVideo/Artwork.tsx
@@ -1,41 +1,56 @@
-import {useRef, useState} from 'react';
-import {Img, interpolate, useCurrentFrame, useVideoConfig} from 'remotion';
+import {useCallback, useState} from 'react';
+import {
+	continueRender,
+	delayRender,
+	interpolate,
+	useCurrentFrame,
+	useVideoConfig,
+} from 'remotion';
 import styled from 'styled-components';
 
-export const Artwork: React.FC<{artFile: String}> = ({artFile}) => {
-	const src = require('../static/artImages/art1.jpg');
+export const Artwork: React.FC<{artFile: string}> = ({artFile}) => {
+	const src = require('../static/artImages/square.jpg');
 	// const src = require('../static/artImages/diptych.jpg');
 
 	const frame = useCurrentFrame();
 	const {height, width, fps, durationInFrames} = useVideoConfig();
-	const artRef = useRef();
 
-	const [state, setState] = useState({
-		artWidth: 1,
-		artHeight: 1,
-		widthRatio: 1,
-		heightRatio: 1,
-	});
+	// const artRef = useRef();
+
+	// const [state, setState] = useState({
+	// 	artWidth: 1,
+	// 	artHeight: 1,
+	// 	widthRatio: 1,
+	// 	heightRatio: 1,
+	// });
 
 	const image = {
 		maxHeight: height * 0.75,
 		maxWidth: width * 0.75,
 	};
 
-	const onLoad = ({target: img}) => {
-		const artWidth = img.offsetWidth;
-		const artHeight = img.offsetHeight;
-		const screenWidth = window.innerWidth;
-		const screenHeight = window.innerHeight;
+	const [handle] = useState(() => delayRender());
 
-		setState({
-			...state,
-			artWidth,
-			artHeight,
-			widthRatio: screenWidth / artWidth,
-			heightRatio: screenHeight / artHeight,
-		});
-	};
+	const onLoad = useCallback(
+		({target: img}) => {
+			// const artWidth = img.offsetWidth;
+			// const artHeight = img.offsetHeight;
+			// const screenWidth = window.innerWidth;
+			// const screenHeight = window.innerHeight;
+
+			// setState({
+			// 	...state,
+			// 	artWidth,
+			// 	artHeight,
+			// 	widthRatio: screenWidth / artWidth,
+			// 	heightRatio: screenHeight / artHeight,
+			// });
+
+			// Continue the delayed render when image loaded
+			continueRender(handle);
+		},
+		[handle]
+	);
 
 	// Zoom using current frame
 	const zoom = interpolate(
@@ -46,7 +61,6 @@ export const Artwork: React.FC<{artFile: String}> = ({artFile}) => {
 			(7 / 8) * durationInFrames, // zoom out starting at 7/8 duration
 			durationInFrames,
 		],
-		// [1, state.widthRatio * 2, state.widthRatio * 2, 1] // Using art width ratio
 		[1, 4, 4, 1] // Using standard scale
 	);
 
@@ -71,7 +85,7 @@ export const Artwork: React.FC<{artFile: String}> = ({artFile}) => {
 			(7 / 8) * durationInFrames,
 			durationInFrames,
 		],
-		[0, 0, 125, 125, 0, 0], // TODO find adequate ratio depending on images aspect ratio
+		[0, 0, 50, 50, 0, 0], // TODO find adequate ratio depending on images aspect ratio
 		{
 			extrapolateRight: 'clamp',
 		}
@@ -86,7 +100,7 @@ export const Artwork: React.FC<{artFile: String}> = ({artFile}) => {
 			(7 / 8) * durationInFrames,
 			durationInFrames,
 		],
-		[0, 0, 0, 125, 125, 0],
+		[0, 0, 0, 50, 50, 0], // pan half the width of the artwork
 		{
 			extrapolateRight: 'clamp',
 		}
@@ -99,7 +113,7 @@ export const Artwork: React.FC<{artFile: String}> = ({artFile}) => {
 		width: 100%;
 	`;
 
-	const StyledImg = styled(Img)`
+	const StyledImg = styled.img`
 		max-height: ${image.maxHeight}px;
 		width: auto;
 		max-width: ${image.maxWidth}px;
@@ -110,19 +124,13 @@ export const Artwork: React.FC<{artFile: String}> = ({artFile}) => {
 			16px 32px 64px rgba(0, 0, 0, 0.2), 4px 8px 4px rgba(0, 0, 0, 0.09),
 			8px 16px 8px rgba(0, 0, 0, 0.09), 16px 32px 16px rgba(0, 0, 0, 0.09),
 			32px 64px 32px rgba(0, 0, 0, 0.09), 64px 128px 64px rgba(0, 0, 0, 0.09);
-		transform: scale(${zoom});
+		transform: scale(${zoom}) translate(-${panX}%, -${panY}%); // interpolate animation with drivers
 		transform-origin: 25% 25%;
-	`;
-
-	const TranslateDiv = styled.div`
-		transform: translate(-${panX}%, -${panY}%);
 	`;
 
 	return (
 		<ArtContainer>
-			<TranslateDiv>
-				<StyledImg ref={artRef} src={src} onLoad={onLoad}></StyledImg>
-			</TranslateDiv>
+			<StyledImg src={src} onLoad={onLoad}></StyledImg>
 		</ArtContainer>
 	);
 };
